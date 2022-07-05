d5b09d0ade4020e393ea1542bbc615b33c122fe0
krzysztoffila <umowy.fila@gmail.com>
2022-07-05 15:09:42 +0200

    Add if loop in script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ let playerMove = 'papier';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 printMessage('Zagrałeś ' + playerMove);
 
-let randomNumber = Math.floor(Math.random() * 12 + 7);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 printMessage(randomNumber);
+
+if(randomNumber == 1){
+	printMessage('Kamień');
+}
+if(randomNumber == 2){
+    printMessage('Papier');
+}
+if(randomNumber == 3){
+    printMessage('Nozyce');
+} 
