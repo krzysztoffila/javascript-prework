9ac72c538d7d9ef0f55c122a2495578ebfc37645
krzysztoffila <umowy.fila@gmail.com>
2022-07-05 14:51:30 +0200

    Add calculation to random numbers(1-3)

--- js/script.js
+++ js/script.js
@@ -2,4 +2,16 @@ let computerMove = `kamień`;
 let playerMove = 'papier';
 
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałeś ' + userMove);
+printMessage('Zagrałeś ' + playerMove);
+
+let randomFraction = Math.random();
+
+printMessage('Wylosowany ułamek to: ' + randomFraction);
+
+let calculation = randomFraction * 3 + 1;
+
+printMessage('Ułamek pomnożony przez 3 i powiększony o 1: ' + calculation);
+
+let roundNumber = Math.floor(calculation);
+
+printMessage('Liczba po zaokrągleniu w dół to: ' + roundNumber);
