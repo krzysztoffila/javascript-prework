b8cdf2d2b172e4d0b8dd41dcda613a622c8a1606
krzysztoffila <umowy.fila@gmail.com>
2022-07-05 14:54:25 +0200

    Testing with random numbers (11-19)

--- js/script.js
+++ js/script.js
@@ -4,14 +4,6 @@ let playerMove = 'papier';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 printMessage('Zagrałeś ' + playerMove);
 
-let randomFraction = Math.random();
+let randomNumber = Math.floor(Math.random() * 12 + 7);
 
-printMessage('Wylosowany ułamek to: ' + randomFraction);
-
-let calculation = randomFraction * 3 + 1;
-
-printMessage('Ułamek pomnożony przez 3 i powiększony o 1: ' + calculation);
-
-let roundNumber = Math.floor(calculation);
-
-printMessage('Liczba po zaokrągleniu w dół to: ' + roundNumber);
+printMessage(randomNumber);
