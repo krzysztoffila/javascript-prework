1eef31452d7bc998b36c7f4b31e0cacc84f65bc6
krzysztoffila <umowy.fila@gmail.com>
2022-07-06 13:34:47 +0200

    Add playerMove in js/script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-printMessage(randomNumber);
-
 let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
 	computerMove = 'Kamień';
-}
-else if(randomNumber == 2){
+}else if(randomNumber == 2){
     computerMove = 'Papier';
-}
-else{
-    computerMove = 'Nozyce';
+}else{
+    computerMove = 'Nożyce';
 } 
 printMessage("Komputer: " + "Mój ruch to " + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: Kamień, 2: Papier, 3: Nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+    playerMove = 'Kamień';
+}else if(playerInput == '2'){
+    playerMove = 'Papier'
+}else if(playerInput == '3'){
+    playerMove = 'Nożyce';
+}else{
+    printMessage('niepoprawny ruch. Wybierz przedział 1-3');
+}
+
+printMessage('Twój ruch to: ' + playerMove);
