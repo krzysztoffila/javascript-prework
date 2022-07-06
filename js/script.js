fe16cb00e2454437b0f69f83a6d48f6beecaf172
krzysztoffila <umowy.fila@gmail.com>
2022-07-06 13:58:26 +0200

    Add turns with score in js/script.js

--- js/script.js
+++ js/script.js
@@ -11,10 +11,7 @@ if(randomNumber == 1){
 printMessage("Komputer: " + "Mój ruch to " + computerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1: Kamień, 2: Papier, 3: Nożyce.');
-
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = 'nieznany ruch';
+let playerMove = 'Nieznany ruch';
 
 if(playerInput == '1'){
     playerMove = 'Kamień';
@@ -23,7 +20,22 @@ if(playerInput == '1'){
 }else if(playerInput == '3'){
     playerMove = 'Nożyce';
 }else{
-    printMessage('niepoprawny ruch. Wybierz przedział 1-3');
-}
-
+    printMessage('nieznany ruch. Wybierz przedział 1-3');
+};
 printMessage('Twój ruch to: ' + playerMove);
+
+//Wygrana Gracza
+if( computerMove == 'Kamień' && playerMove == 'Papier' || computerMove == 'Papier' && playerMove == 'Nożyce'){
+    printMessage('Ty wygrywasz!');
+}
+//Wygrana Komputera
+else if( computerMove == 'Papier' && playerMove == 'Kamień' || computerMove == 'Nożyce' && playerMove == 'Papier'){
+    printMessage('Wygrywa Komputer');
+}
+//Remis
+else if(computerMove == 'Papier' && playerMove == 'Papier' || computerMove == 'Kamień' && playerMove == 'Kamień' || computerMove == 'Nożyce' && playerMove == 'Nożyce'){
+    printMessage('REMIS');
+}
+else{
+    printMessage('Niepoprawny ruch GRACZA. Wybierz przedział 1-3');
+};
