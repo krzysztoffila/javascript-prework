19baf37aaa4272b54348e289fc52793b301d1803
krzysztoffila <umowy.fila@gmail.com>
2022-07-08 16:54:03 +0200

    Short the loop if..else(function displayResult) in js/script.js

--- js/script.js
+++ js/script.js
@@ -16,10 +16,10 @@ function getMoveName(argMoveId){
 function displayResult(argComputerMove, argPlayerMove){
     printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 
-    if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+    if( argComputerMove == 'kamień' && argPlayerMove == 'papier' || argComputerMove =='papier' && argPlayerMove =='nożyce' || argComputerMove =='nożyce' && argPlayerMove =='kamień'){
     printMessage('Ty wygrywasz!');
     }
-    else if ( argComputerMove == 'kamień' && argPlayerMove =='nożyce'){
+    else if ( argComputerMove == 'kamień' && argPlayerMove =='nożyce' || argComputerMove =='papier' && argPlayerMove =='kamień' || argComputerMove =='nożyce' && argPlayerMove =='papier'){
     printMessage('Ja wygrywam!');
     }
     else if (argComputerMove == argPlayerMove){
@@ -27,18 +27,6 @@ function displayResult(argComputerMove, argPlayerMove){
     }
     else if (argPlayerMove =='nieznany ruch'){
     printMessage('Fail');
-    }
-    else if (argComputerMove =='papier' && argPlayerMove =='nożyce'){
-    printMessage('Ty wygrywasz!');
-    }
-    else if (argComputerMove =='papier' && argPlayerMove =='kamień'){
-    printMessage('Ja wygrywam');
-    }
-    else if (argComputerMove =='nożyce' && argPlayerMove =='kamień'){
-    printMessage('Ty wygrywasz!');
-    }
-    else if (argComputerMove =='nożyce' && argPlayerMove =='papier'){
-    printMessage('Ja wygrywam!');
     }
 }
 let randomNumber = Math.floor(Math.random() * 3 + 1);
