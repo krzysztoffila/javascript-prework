c5905ae915d57c08cd8652cdb736dc9a050b3fbb
krzysztoffila <umowy.fila@gmail.com>
2022-07-08 16:47:28 +0200

    Fix displayResult

--- js/script.js
+++ js/script.js
@@ -13,32 +13,6 @@ function getMoveName(argMoveId){
         return 'nieznany ruch';
     }
 }
-
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-let computerMove = getMoveName(randomNumber);
-printMessage('Mój ruch to ' + computerMove);
-
-let playerInput = prompt('Wybierz swój ruch! 1: papier, 2: kamień, 3: nożyce.');
-let playerMove = getMoveName(playerInput);
-printMessage('Twój ruch to: ' + playerMove);
-/*
-//Wygrana Gracza
-if( computerMove == 'Kamień' && playerMove == 'Papier' || computerMove == 'Papier' && playerMove == 'Nożyce' || computerMove == 'Nożyce' && playerMove == 'Kamień'){
-    printMessage('Ty wygrywasz!');
-}
-//Wygrana Komputera
-else if( computerMove == 'Papier' && playerMove == 'Kamień' || computerMove == 'Nożyce' && playerMove == 'Papier' || computerMove == 'Kamień' && playerMove == 'Nożyce'){
-    printMessage('Wygrywa Komputer');
-}
-//Remis
-else if(argComputerMove == argPlayerMove){
-    printMessage('REMIS');
-}
-else if(playerInput > 3){
-    printMessage('Niepoprawny ruch GRACZA. Wybierz przedział 1-3');
-};
-*/
-
 function displayResult(argComputerMove, argPlayerMove){
     printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 
@@ -66,7 +40,15 @@ function displayResult(argComputerMove, argPlayerMove){
     else if (argComputerMove =='nożyce' && argPlayerMove =='papier'){
     printMessage('Ja wygrywam!');
     }
-return displayResult(computerMove, playerMove);
 }
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+let computerMove = getMoveName(randomNumber);
+printMessage('Mój ruch to ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: papier, 2: kamień, 3: nożyce.');
+let playerMove = getMoveName(playerInput);
+printMessage('Twój ruch to: ' + playerMove);
+
+displayResult(playerMove, computerMove);
 
 
