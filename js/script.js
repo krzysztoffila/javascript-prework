7caeada585adc9a2fad2768ede20695908610d37
krzysztoffila <umowy.fila@gmail.com>
2022-07-10 16:53:28 +0200

    Add fontawesome

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function playGame(playerInput){
     clearMessages();
     let randomNumber = Math.floor(Math.random() * 3 + 1);
     let computerMove = getMoveName(randomNumber);
-    printMessage('Mój ruch to ' + computerMove);
+    printMessage('Mój ruch to: ' + computerMove);
 
     // let playerInput = prompt('Wybierz swój ruch! 1: papier, 2: kamień, 3: nożyce.');
     let playerMove = getMoveName(playerInput);
@@ -10,13 +10,13 @@ function playGame(playerInput){
 
     function getMoveName(argMoveId){
         if(argMoveId == 1){
-            return 'kamień';
+            return 'Kamień';
         } 
         else if(argMoveId == 2){
-            return 'papier';
+            return 'Papier';
         }
         else if(argMoveId == 3){
-            return 'nożyce';
+            return 'Nożyce';
         }
         else {
             printMessage('Nie znam ruchu o id ' + argMoveId + '.');
@@ -26,10 +26,10 @@ function playGame(playerInput){
     function displayResult(argComputerMove, argPlayerMove){
         printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 
-        if( argComputerMove == 'kamień' && argPlayerMove == 'papier' || argComputerMove =='papier' && argPlayerMove =='nożyce' || argComputerMove =='nożyce' && argPlayerMove =='kamień'){
+        if( argComputerMove == 'Kamień' && argPlayerMove == 'Papier' || argComputerMove =='Papier' && argPlayerMove =='Nożyce' || argComputerMove =='Nożyce' && argPlayerMove =='Kamień'){
         printMessage('Ty wygrywasz!');
         }
-        else if ( argComputerMove == 'kamień' && argPlayerMove =='nożyce' || argComputerMove =='papier' && argPlayerMove =='kamień' || argComputerMove =='nożyce' && argPlayerMove =='papier'){
+        else if ( argComputerMove == 'Kamień' && argPlayerMove =='Nożyce' || argComputerMove =='Papier' && argPlayerMove =='Kamień' || argComputerMove =='Nożyce' && argPlayerMove =='Papier'){
         printMessage('Ja wygrywam!');
         }
         else if (argComputerMove == argPlayerMove){
